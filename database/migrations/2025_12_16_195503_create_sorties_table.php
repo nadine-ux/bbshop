21d2ea33796d46c04db4c0a3d69b5b49269db63d
nadine-ux <nadinezerouali3@gmail.com>
2026-01-08 20:29:53 +0100

    fix sortie

--- database/migrations/2025_12_16_195503_create_sorties_table.php
+++ database/migrations/2025_12_16_195503_create_sorties_table.php
@@ -6,28 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-public function up(): void
-{
-    Schema::create('article_sortie', function (Blueprint $table) {
-        $table->id();
-        $table->foreignId('sortie_id')->constrained()->onDelete('cascade');
-        $table->foreignId('article_id')->constrained()->onDelete('cascade');
-        $table->integer('quantite_cartons')->default(0);
-        $table->integer('quantite_pieces')->default(0);
-        $table->integer('quantite_total')->default(0); // toujours en pièces
-        $table->timestamps();
-    });
-}
+    public function up(): void
+    {
+        Schema::create('sorties', function (Blueprint $table) {
+            $table->id();
+            $table->string('destination');
+            $table->string('motif')->nullable();
+            $table->text('commentaire')->nullable();
+            $table->timestamps();
+        });
+    }
 
-
-    
-
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('sorties');
