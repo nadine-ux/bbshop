16d77d4fa8b71fe6288f0197a594a594be6a20c3
nadine-ux <nadinezerouali3@gmail.com>
2026-01-08 20:46:27 +0100

    userfactfkkl

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -7,20 +7,16 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
- */
 class UserFactory extends Factory
 {
     protected $model = User::class;
-    
     protected static ?string $password;
 
     public function definition(): array
     {
         return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
+            'name' => 'User ' . rand(1000, 9999),
+            'email' => 'user' . rand(1000, 9999) . '@example.com',
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
